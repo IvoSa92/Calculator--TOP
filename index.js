57ac0e543199e524b267e5133eba447dc4ae518a
IvoSa92 <ivo_sauter@web.de>
2023-11-14 21:01:03 +0100

    - added several variables -added event listeners fpr the number buttons, operator buttons, clear button and equal button. -tried to get the calculator work with adding first. little bit frustrating, lets see tomorrow :D

--- index.js
+++ index.js
@@ -21,3 +21,68 @@ function operate(num1, num2, operator) {
   sum = operator(num1, num2);
   return sum;
 }
+
+// variables:
+
+const display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number-button");
+const operatorButtons = document.querySelectorAll(".operator-button");
+const deleteButton = document.querySelector(".delete-button");
+const equalButton = document.querySelector(".equal-button");
+const clearButton = document.querySelector(".clear-button");
+
+// variables
+
+let currentOperator = "";
+let currentNumber1String = "";
+let currentNumber2String = "";
+let currentNumber1 = "";
+let currentNumber2 = "";
+let result = 0;
+
+// numbers event listener
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (currentOperator == "") {
+      currentNumber1String += this.innerHTML;
+      currentNumber1 = parseFloat(currentNumber1String);
+    } else if (currentOperator !== "") {
+      currentNumber2String += this.innerHTML;
+      currentNumber2 = parseFloat(currentNumber2String);
+    }
+    display.innerHTML = currentNumber1 + currentOperator + currentNumber2;
+  });
+});
+
+//operator event listener
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    currentOperator = this.innerHTML;
+    display.innerHTML = currentNumber1 + currentOperator;
+  });
+});
+
+// operating event listener
+
+equalButton.addEventListener("click", function () {
+  if (currentOperator == "+") {
+    num1 = currentNumber1;
+    num2 = currentNumber2;
+    result = num1 + num2;
+    display.innerHTML = "= " + result;
+  }
+});
+
+// clear function:
+function clear() {
+  currentNumber1 = "";
+  currentNumber2 = "";
+  currentNumber1String = "";
+  currentNumber2String = "";
+  currentOperator = "";
+  display.innerHTML = "";
+}
+
+clearButton.addEventListener("click", clear);
